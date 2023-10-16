628ade59637c2bbe99cbd69a6739346b76255f94
vasuvs45 <vasu.vs45@gmail.com>
2023-10-15 20:07:34 -0400

    Third_Commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-$j^)9pcs3@&i@dhndwtji%@i&2xk#al6()+*2ban&gw!51=w##
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['127.0.0.1']
 ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
