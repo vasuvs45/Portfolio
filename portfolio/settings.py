920466247d407f0542a2dffd88e7a61d68c1e1da
vasuvs45 <vasu.vs45@gmail.com>
2023-10-16 16:46:33 -0400

    Seventh_Commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -87,8 +87,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'BVEqcfZVr9jbk87iU7ao',
+        'HOST':'containers-us-west-82.railway.app',
+        'PORT':'7477',
     }
 }
 
